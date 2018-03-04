a1154df9720e49800cd314d2391d14b1a735bbd4
beginor <beginor@qq.com>
2018-03-04 16:01:40 +0800

    fix version error

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Version Info:
 
-- Jexus 5.8.2.21
+- Jexus 5.8.3.0
 - Mono 5.10.0.140
 - Ubuntu 16.04
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --no-cache --rm -t beginor/jexus:5.8.3 . 
+docker build --no-cache --rm -t beginor/jexus:5.8.3.0 . 
