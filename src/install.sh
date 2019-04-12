bbf798249539d33a81921870284f7166646fa95e
beginor <beginor@qq.com>
2019-04-12 17:36:03 +0800

    update install script

--- src/install.sh
+++ src/install.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
-### every exit != 0 fails the script
-set -e
+#!/bin/bash -e
 apt-get update && apt upgrade -y
 # install wget to download jexus, curl for healthcheck.
-apt-get install -y wget curl
+apt-get install -y --no-install-recommends wget curl
 wget https://linuxdot.net/down/jexus-5.8.3.tar.gz
 tar -zxf jexus-5.8.3.tar.gz
 jexus-5.8.3/install
